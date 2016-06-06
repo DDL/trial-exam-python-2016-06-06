d29dd9cd4e54ab367476bc307c55326cdf642e9f
David <doczi.david.daci@gmail.com>
2016-06-06 09:42:20 +0200

    1.py

--- 1.py
+++ 1.py
@@ -2,9 +2,12 @@
 # and returns a new list with all it's element value doubled.
 # It should raise an error if the parameter is not a list
 
+testList = []
 n = [1, 2, 3, 4, 5]
 
 def double_list(input_list):
+    if type(input_list) != type(testList):
+        raise Error
     newList = []
     for item in input_list:
         item *= 2
